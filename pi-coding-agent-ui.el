8c62696be4fa5c9f1d92bd6c3cee080e3b71fa6e
Daniel Nouri <daniel.nouri@gmail.com>
2026-08-27 21:57:01 +0200

    Release v2.9.0

--- pi-coding-agent-ui.el
+++ pi-coding-agent-ui.el
@@ -2049,7 +2049,7 @@ warnings for missing dependencies."
 
 ;;;; Startup Header
 
-(defconst pi-coding-agent-version "2.8.0"
+(defconst pi-coding-agent-version "2.9.0"
   "Version of pi-coding-agent.")
 
 (defconst pi-coding-agent--version-probe-delay 0.1
